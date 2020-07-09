7144aa3314bfc050ac2fa201f1a72264a9ad14a7
Mansoor Ahmed <mansoor.ahmed@knowledgebiz.pt>
2020-07-09 17:07:25 +0100

    🤏added examples

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
+import "./styles/style.css"; //css import
+
 //your code here
+/**@description JS Doc test
+ */
+let evens = [2, 4, 6, 8];
+var odds = evens.map(v => v + 1); //es6 code will be transpiled by Babel transpiler
+
+console.log(odds);
+let i = 0;
+document.getElementById("btn").addEventListener("click", () => {
+	document.getElementById("counter").innerHTML = i++;
+	console.log("clicked");
+});
